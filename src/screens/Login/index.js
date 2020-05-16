6bc5777ef564b821195edb33ee7e301e262d4316
Bruno Andreo <brunolino2026@gmail.com>
2020-05-16 20:39:51 -0300

    Criando tela de login

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,13 +1,77 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Image, TextInput, TouchableOpacity, Text, StyleSheet, StatusBar } from 'react-native';
+
+import LogoPath from '../../assets/diary-logo-claro.png';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#123456',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    resizeMode: 'stretch',
+    width: 300,
+    height: 150,
+    marginBottom: 50
+  },
+  input: {
+    width: 250,
+    height: 35,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+    marginBottom: 10,
+    borderRadius: 4,
+    padding: 10
+  },
+  button: {
+    width: 250,
+    height: 35,
+    backgroundColor: '#00cc00',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+    marginTop: 10,
+    borderRadius: 4
+  },
+  textButton: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16
+  }
+});
 
 export default function Login(props) {
     return (
-        <View>
-            <Text>Olá Login</Text>
-            <Button
-                title='Go to Home'
-                onPress={() => props.navigation.navigate('Home')} />
-        </View>
+        <>
+          <StatusBar barStyle="light-content" backgroundColor="#123456" />
+          <View style={styles.container}>
+            <Image
+              source={LogoPath}
+              style={styles.logo}
+            />
+
+            <TextInput
+              placeholder="User"
+              autoCapitalize="none"
+              autoCompleteType="off"
+              style={styles.input}
+            />
+
+            <TextInput
+              placeholder="Password"
+              secureTextEntry
+              style={styles.input}
+            />
+
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.textButton}>Entrar</Text>
+            </TouchableOpacity>
+          </View>
+        </>
     );
 }
