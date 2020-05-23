833c306a5d26417ac9ec153da0fc52b373706bc1
Bruno Andreo <brunolino2026@gmail.com>
2020-05-23 12:57:44 -0300

    Finalizando Menu lateral

--- src/routes/DrawerRoute.js
+++ src/routes/DrawerRoute.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { createDrawerNavigator } from 'react-navigation-drawer';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Screens from '../lib/Screens';
 import Colors from '../styles/Colors';
@@ -25,10 +25,44 @@ export default createDrawerNavigator({
     screen: Home,
     navigationOptions: () => ({
       drawerIcon: <ItemIcon name="home" />,
-      drawerLabel: <Item name="Home" />
+      drawerLabel: <Item name="Tela Inicial" />
     }),
   },
-  Login
+  Visitas: {
+    screen: Home,
+    navigationOptions: () => ({
+      drawerIcon: <ItemIcon name="location-city" />,
+      drawerLabel: <Item name="Visitas" />
+    }),
+  },
+  Reunioes: {
+    screen: Home,
+    navigationOptions: () => ({
+      drawerIcon: <ItemIcon name="work" />,
+      drawerLabel: <Item name="Reuniões" />
+    }),
+  },
+  Sincronizacao: {
+    screen: Home,
+    navigationOptions: () => ({
+      drawerIcon: <ItemIcon name="sync" />,
+      drawerLabel: <Item name="Sincronização" />
+    }),
+  },
+  Ajuda: {
+    screen: Home,
+    navigationOptions: () => ({
+      drawerIcon: <ItemIcon name="help" />,
+      drawerLabel: <Item name="Ajuda" />
+    }),
+  },
+  Sair: {
+    screen: Login,
+    navigationOptions: () => ({
+      drawerIcon: <ItemIcon name="exit-to-app" />,
+      drawerLabel: <Item name="Sair" />
+    }),
+  },
 }, {
   contentComponent: MenuLateral,
   initialRouteName: Screens.home,
