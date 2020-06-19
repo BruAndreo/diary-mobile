297085881059ef48d12e788cd541fbbf88d03b8d
Bruno Andreo <brunolino2026@gmail.com>
2020-06-19 15:57:17 -0300

    Aumentando mock de compromissos e removendo tsconfig

--- src/services/CompromissosDoDiaMock.js
+++ src/services/CompromissosDoDiaMock.js
@@ -1,10 +1,26 @@
 import moment from "moment";
 import TypesCompromissos from '../lib/TiposDeCompromissos';
 
-export default [{
+export default [
+  //segunda
+  {
+    id: '3',
+    type: TypesCompromissos.VISIT,
+    at: moment(1).hour({hour:10, minute:0, second:0, millisecond:0}),
+    nomeEmpresa: "Impacta",
+    nomeResponsavel: "Renato",
+    address: {
+      cep: "09990730",
+      street: "Av. Paulista",
+      number: "1234",
+      city: "São Paulo",
+      uf: "SP",
+    },
+  },
+  {
     id: '1',
     type: TypesCompromissos.MEETING,
-    at: moment('2020-05-27 15:00:00'),
+    at: moment(1).hour({hour:16, minute:0, second:0, millisecond:0}),
     nomeEmpresa: "Impacta",
     nomeResponsavel: "Renato",
     address: {
@@ -18,7 +34,179 @@ export default [{
   {
     id: '2',
     type: TypesCompromissos.VISIT,
-    at: moment('2020-05-27 16:00:00'),
+    at: moment(1).hour({hour:17, minute:0, second:0, millisecond:0}),
+    nomeEmpresa: "Impacta",
+    nomeResponsavel: "Renato",
+    address: {
+      cep: "09990730",
+      street: "Av. Paulista",
+      number: "1234",
+      city: "São Paulo",
+      uf: "SP",
+    },
+  },
+  //terça
+  {
+    id: '4',
+    type: TypesCompromissos.VISIT,
+    at: moment(2).hour({hour:10, minute:0, second:0, millisecond:0}),
+    nomeEmpresa: "Impacta",
+    nomeResponsavel: "Renato",
+    address: {
+      cep: "09990730",
+      street: "Av. Paulista",
+      number: "1234",
+      city: "São Paulo",
+      uf: "SP",
+    },
+  },
+  {
+    id: '5',
+    type: TypesCompromissos.MEETING,
+    at: moment(2).hour({hour:13, minute:0, second:0, millisecond:0}),
+    nomeEmpresa: "Impacta",
+    nomeResponsavel: "Renato",
+    address: {
+      cep: "09990730",
+      street: "Av. Paulista",
+      number: "1234",
+      city: "São Paulo",
+      uf: "SP",
+    },
+  },
+  {
+    id: '6',
+    type: TypesCompromissos.MEETING,
+    at: moment(2).hour({hour:15, minute:0, second:0, millisecond:0}),
+    nomeEmpresa: "Impacta",
+    nomeResponsavel: "Renato",
+    address: {
+      cep: "09990730",
+      street: "Av. Paulista",
+      number: "1234",
+      city: "São Paulo",
+      uf: "SP",
+    },
+  },
+  //quarta
+  {
+    id: '7',
+    type: TypesCompromissos.VISIT,
+    at: moment(3).hour({hour:13, minute:0, second:0, millisecond:0}),
+    nomeEmpresa: "Impacta",
+    nomeResponsavel: "Renato",
+    address: {
+      cep: "09990730",
+      street: "Av. Paulista",
+      number: "1234",
+      city: "São Paulo",
+      uf: "SP",
+    },
+  },
+  {
+    id: '8',
+    type: TypesCompromissos.VISIT,
+    at: moment(3).hour({hour:14, minute:0, second:0, millisecond:0}),
+    nomeEmpresa: "Impacta",
+    nomeResponsavel: "Renato",
+    address: {
+      cep: "09990730",
+      street: "Av. Paulista",
+      number: "1234",
+      city: "São Paulo",
+      uf: "SP",
+    },
+  },
+  {
+    id: '9',
+    type: TypesCompromissos.VISIT,
+    at: moment(3).hour({hour:20, minute:0, second:0, millisecond:0}),
+    nomeEmpresa: "Impacta",
+    nomeResponsavel: "Renato",
+    address: {
+      cep: "09990730",
+      street: "Av. Paulista",
+      number: "1234",
+      city: "São Paulo",
+      uf: "SP",
+    },
+  },
+  // quinta
+  {
+    id: '10',
+    type: TypesCompromissos.MEETING,
+    at: moment(4).hour({hour:10, minute:0, second:0, millisecond:0}),
+    nomeEmpresa: "Impacta",
+    nomeResponsavel: "Renato",
+    address: {
+      cep: "09990730",
+      street: "Av. Paulista",
+      number: "1234",
+      city: "São Paulo",
+      uf: "SP",
+    },
+  },
+  {
+    id: '11',
+    type: TypesCompromissos.MEETING,
+    at: moment(4).hour({hour:16, minute:0, second:0, millisecond:0}),
+    nomeEmpresa: "Impacta",
+    nomeResponsavel: "Renato",
+    address: {
+      cep: "09990730",
+      street: "Av. Paulista",
+      number: "1234",
+      city: "São Paulo",
+      uf: "SP",
+    },
+  },
+  {
+    id: '12',
+    type: TypesCompromissos.VISIT,
+    at: moment(4).hour({hour:17, minute:0, second:0, millisecond:0}),
+    nomeEmpresa: "Impacta",
+    nomeResponsavel: "Renato",
+    address: {
+      cep: "09990730",
+      street: "Av. Paulista",
+      number: "1234",
+      city: "São Paulo",
+      uf: "SP",
+    },
+  },
+  // sexta
+  {
+    id: '13',
+    type: TypesCompromissos.MEETING,
+    at: moment(5).hour({hour:9, minute:0, second:0, millisecond:0}),
+    nomeEmpresa: "Impacta",
+    nomeResponsavel: "Renato",
+    address: {
+      cep: "09990730",
+      street: "Av. Paulista",
+      number: "1234",
+      city: "São Paulo",
+      uf: "SP",
+    },
+  },
+  {
+    id: '14',
+    type: TypesCompromissos.MEETING,
+    at: moment(5).hour({hour:11, minute:0, second:0, millisecond:0}),
+    nomeEmpresa: "Impacta",
+    nomeResponsavel: "Renato",
+    address: {
+      cep: "09990730",
+      street: "Av. Paulista",
+      number: "1234",
+      city: "São Paulo",
+      uf: "SP",
+    },
+  },
+  {
+    id: '15',
+    type: TypesCompromissos.VISIT,
+    at: moment(5).hour({hour:14, minute:0, second:0, millisecond:0}),
     nomeEmpresa: "Impacta",
     nomeResponsavel: "Renato",
     address: {
