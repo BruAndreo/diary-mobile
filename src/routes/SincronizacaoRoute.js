91c7d8d06b930a01b7c5d4b96278309a1e13f7cb
BruAndreo <brunolino2026@gmail.com>
2020-08-11 23:02:48 -0300

    Removing code smells in route

--- src/routes/SincronizacaoRoute.js
+++ src/routes/SincronizacaoRoute.js
@@ -1,46 +1,7 @@
 import React from 'react';
-import { View, StatusBar } from 'react-native';
-//import { createStackNavigator } from 'react-navigation-stack';
-import Icon from 'react-native-vector-icons/MaterialIcons';
-import Screens from '../lib/Screens';
-import Colors from '../styles/Colors';
-
-import Sincronizacao from '../screens/Sincronizacao';
-/*
-Icon.loadFont();
-
-const MenuIcon = (props) => (
-  <>
-    <StatusBar barStyle="light-content" backgroundColor="#123456" />
-    <View>
-      <Icon onPress={() => props.navigation.openDrawer()} name="menu" size={30} color={Colors.white} style={{marginLeft: 10}} />
-    </View>
-  </>
-);
-
-export default createStackNavigator({
-  Sincronizacao: {
-    screen: Sincronizacao,
-    navigationOptions: ({ navigation }) => ({
-      title: 'Sincronização',
-      drawerLockMode: 'unlocked',
-      headerLeft: () => <MenuIcon navigation={navigation} />
-    })
-  }
-}, {
-  initialRouteName: Screens.sincronizacao,
-  defaultNavigationOptions: {
-    headerTitleAlign: 'center',
-    headerStyle: {
-      backgroundColor: Colors.primaryColor,
-    },
-    headerTintColor: Colors.white,
-  },
-});
-*/
-
 import { createStackNavigator } from '@react-navigation/stack';
 import headerStyled from '../components/Menu/HeaderStyled';
+import Sincronizacao from '../screens/Sincronizacao';
 
 const Stack = createStackNavigator();
 
--- src/routes/VisitasRoute.js
+++ src/routes/VisitasRoute.js
@@ -1,46 +1,7 @@
 import React from 'react';
-import { View, StatusBar } from 'react-native';
-//import { createStackNavigator } from 'react-navigation-stack';
-import Icon from 'react-native-vector-icons/MaterialIcons';
-import Screens from '../lib/Screens';
-import Colors from '../styles/Colors';
-
-import Visitas from '../screens/Visitas';
-
-Icon.loadFont();
-/*
-const MenuIcon = (props) => (
-  <>
-    <StatusBar barStyle="light-content" backgroundColor="#123456" />
-    <View>
-      <Icon onPress={() => props.navigation.openDrawer()} name="menu" size={30} color={Colors.white} style={{marginLeft: 10}} />
-    </View>
-  </>
-);
-
-export default createStackNavigator({
-  Visitas: {
-    screen: Visitas,
-    navigationOptions: ({ navigation }) => ({
-      title: 'Visitas',
-      drawerLockMode: 'unlocked',
-      headerLeft: () => <MenuIcon navigation={navigation} />
-    })
-  }
-}, {
-  initialRouteName: Screens.visitas,
-  defaultNavigationOptions: {
-    headerTitleAlign: 'center',
-    headerStyle: {
-      backgroundColor: Colors.primaryColor,
-    },
-    headerTintColor: Colors.white,
-  },
-});
-*/
-
 import { createStackNavigator } from '@react-navigation/stack';
 import headerStyled from '../components/Menu/HeaderStyled';
+import Visitas from '../screens/Visitas';
 
 const Stack = createStackNavigator();
 
