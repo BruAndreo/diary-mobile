f7b0f527ef1b92f04fc6347f5165ad0a691fb08e
BruAndreo <brunolino2026@gmail.com>
2020-08-18 21:31:48 -0300

    feat: add card of meeting data

--- src/screens/Detalhes/index.js
+++ src/screens/Detalhes/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import moment from 'moment';
 import { Styles } from './style';
 import Compromissos from '../../app/Compromissos';
 import TypesCompromissos from '../../lib/TiposDeCompromissos';
@@ -29,11 +31,71 @@ class Detalhes extends Component {
     return this.state.compromisso.type === TypesCompromissos.VISIT;
   }
 
+  getTypeString() {
+    return this.isVisit() ? 'Visita' : 'Reunião';
+  }
+
+  getDay(compromisso) {
+    return moment().isSame(compromisso.at, 'day') ? 'Hoje' : compromisso.at.format('D/MM')
+  }
+
+  getEndereco(address) {
+    return `${address.street}, ${address.number} - ${address.cep} \n ${address.city}/${address.uf}`;
+  }
+
   render() {
+    const compromisso = this.state.compromisso;
+    const address = compromisso.address;
+
     return (
       <View>
         <View style={this.isVisit() ? Styles.visit : Styles.meeting}>
-          <Text style={Styles.textMiniHeader}>{this.isVisit() ? 'Visita': 'Reunião'}</Text>
+          <Text style={Styles.textMiniHeader}>{this.getTypeString()}</Text>
+        </View>
+
+        <View style={Styles.container}>
+          <View style={Styles.cardDados}>
+            <Text style={Styles.titleCard}>DADOS DA {this.getTypeString().toUpperCase()}</Text>
+
+            <View style={Styles.iconName}>
+              <Icon name='location-city' size={22} color={'#123456'} />
+              <Text style={Styles.label}>Empresa:</Text>
+              <Text style={Styles.value}>{compromisso.nomeEmpresa}</Text>
+            </View>
+
+            <View style={Styles.iconName}>
+              <Icon name='person' size={22} color={'#123456'} />
+              <Text style={Styles.label}>Responsável:</Text>
+              <Text style={Styles.value}>{compromisso.nomeResponsavel}</Text>
+            </View>
+
+            <View style={Styles.iconName}>
+              <Icon name='event' size={22} color={'#123456'} />
+              <Text style={Styles.label}>Data:</Text>
+              <Text style={Styles.value}>{this.getDay(compromisso)}</Text>
+            </View>
+
+            <View style={Styles.iconName}>
+              <Icon name='schedule' size={22} color={'#123456'} />
+              <Text style={Styles.label}>Horário:</Text>
+              <Text style={Styles.value}>{compromisso.hour}</Text>
+            </View>
+
+            <View style={Styles.iconName}>
+              <Icon name='location-on' size={22} color={'#123456'} />
+              <Text style={Styles.label}>Endereço:</Text>
+              <Text style={Styles.valueAddress}>{this.getEndereco(new Object(address))}</Text>
+            </View>
+
+          </View>
+
+          <View>
+            <Text>Ir com...</Text>
+          </View>
+
+          <View>
+            <Text>Formulário ou Anotações</Text>
+          </View>
         </View>
       </View>
     );
--- src/screens/Detalhes/style.js
+++ src/screens/Detalhes/style.js
@@ -15,5 +15,43 @@ export const Styles = StyleSheet.create({
     color: Colors.white,
     fontWeight: 'bold',
     textAlign: 'center'
+  },
+  container: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  cardDados: {
+    width: 380,
+    height: 300,
+    backgroundColor: Colors.white,
+    marginTop: 20,
+    padding: 20,
+    borderRadius: 8
+  },
+  titleCard: {
+    color: Colors.primaryDark,
+    fontSize: 18
+  },
+  iconName: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 35,
+    marginTop: 10,
+  },
+  label: {
+    color: Colors.primaryColor,
+    fontSize: 16,
+    marginLeft: 5
+  },
+  value: {
+    color: Colors.primaryDark,
+    fontSize: 20,
+    marginLeft: 15
+  },
+  valueAddress: {
+    color: Colors.primaryDark,
+    fontSize: 16,
+    marginLeft: 15
   }
 });
