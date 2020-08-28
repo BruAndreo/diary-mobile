63dd5276c0df0c26266d9fbfe230aafa7a8067f4
BruAndreo <brunolino2026@gmail.com>
2020-08-28 09:08:14 -0300

    feat: Abrindo tela de detalhes de outras fontes

--- src/routes/ReunioesRoute.js
+++ src/routes/ReunioesRoute.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import headerStyled from '../components/Menu/HeaderStyled';
 import Reunioes from '../screens/Reunioes';
+import Detalhes from '../screens/Detalhes';
 
 const Stack = createStackNavigator();
 
@@ -9,6 +10,7 @@ export default function ReunioesRoute() {
   return (
     <Stack.Navigator initialRouteName='Reunioes'>
       <Stack.Screen name="Reunioes" component={Reunioes} options={({navigation}) => headerStyled(navigation, 'Reuniões')} />
+      <Stack.Screen name="Detalhes" component={Detalhes} options={({navigation}) => headerStyled(navigation, 'Detalhes', false)} />
     </Stack.Navigator>
   );
 }
--- src/routes/VisitasRoute.js
+++ src/routes/VisitasRoute.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import headerStyled from '../components/Menu/HeaderStyled';
 import Visitas from '../screens/Visitas';
+import Detalhes from '../screens/Detalhes';
 
 const Stack = createStackNavigator();
 
@@ -9,6 +10,7 @@ export default function VisitasRoute() {
   return (
     <Stack.Navigator initialRouteName='Visitas'>
       <Stack.Screen name="Visitas" component={Visitas} options={({navigation}) => headerStyled(navigation, 'Visitas')} />
+      <Stack.Screen name="Detalhes" component={Detalhes} options={({navigation}) => headerStyled(navigation, 'Detalhes', false)} />
     </Stack.Navigator>
   );
 }
