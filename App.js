4b45b2d80ee20314bc9f581d1ad3426576ace5fa
Bruno Andreo <brunolino2026@gmail.com>
2020-10-08 21:51:53 -0300

    feat: disabled warnings

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import 'react-native-gesture-handler';
 import moment from 'moment-timezone';
-import {decode, encode} from 'base-64'
+import {decode, encode} from 'base-64';
+
+console.disableYellowBox = true;
 
 import AuthRoute from './src/routes/AuthRoute';
 
