3dc8a2e8190b38f592b1bd3a66d7866ed9c1e095
Bruno Andreo <brunolino2026@gmail.com>
2020-10-08 21:42:02 -0300

    feat: Alterando tipos de garantia e produtos

--- src/services/TiposGarantiaMock.js
+++ src/services/TiposGarantiaMock.js
@@ -1,14 +1,34 @@
 module.exports = [
   {
     idTipoGarantia: 1,
-    nome: 'Imóvel'
+    nome: 'Alienação Fiduciária Veículos'
   },
   {
     idTipoGarantia: 2,
-    nome: 'Veículo'
+    nome: 'Alienação Fiduciária Bens'
   },
   {
     idTipoGarantia: 3,
-    nome: 'Valor de entrada'
+    nome: 'Alienação Fiduciária Imóveis'
+  },
+  {
+    idTipoGarantia: 4,
+    nome: 'Aval Pessoa Física'
+  },
+  {
+    idTipoGarantia: 5,
+    nome: 'Aval Pessoa Jurídica'
+  },
+  {
+    idTipoGarantia: 6,
+    nome: 'Cessão de Direitos Creditórios'
+  },
+  {
+    idTipoGarantia: 7,
+    nome: 'Hipoteca'
+  },
+  {
+    idTipoGarantia: 8,
+    nome: 'Penhor'
   },
 ];
